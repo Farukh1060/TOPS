a82118cfbb482d4247d4396dfde1b0e7b6129102
Farukh1060 <132422618+Farukh1060@users.noreply.github.com>
2024-05-24 22:37:19 +0530

    Add files via upload

---
+++ FRONTEND/PRACTIC/JAVASCRIPT/17_get_set.js
@@ -0,0 +1,27 @@
+//getter and setter allow to control how object properties are accessed and modify
+//getter and setter can be use to validate data before it is set to an object
+let person ={
+    firstname : "masters",
+    lastname : "cam",
+
+
+    // getname : function(){
+    //     console.log(this.firstname.toUpperCase());
+    // }
+
+    get getname(){
+        return this.firstname;
+    },
+
+    set setname (change){
+        return this.firstname = change.toUpperCase();
+    }
+
+}
+console.log(person.firstname);   
+console.log(person.getname);
+//person.getname();   ////////call method to axcess property 
+person.setname = "solid";
+
+
+console.log(person.firstname);         
---
+++ FRONTEND/PRACTIC/JAVASCRIPT/21_DOM.js
@@ -0,0 +1,8 @@
+// DOM document objec model is an application programing 
+//intrface for manipulation html and xml document
+// dom creat tree structure where in each node is an object 
+// DOM have both property and method
+
+// can change
+// html element,attribute,css style,remove or add html elemnt
+//react to all existing html event or creat new event
