a6699b8dbfd599c385a95c21cf00b662621406f8
shaikh farukh <shaikhfarukh60@gmail.com>
2024-11-24 17:03:45 +0530

    react_practic

--- FRONTEND/CLASS_ASS/REACT/Ecommerce-website/src/App.jsx
+++ FRONTEND/CLASS_ASS/REACT/Ecommerce-website/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 
 import Navbar from './Components/Navbar/Navbar'
 import Slider from './Components/Slider/slider'
+import sale from "./assets/sale.png"
+import shopping from "./assets/shopping.png"
+import women from "./assets/women.png"
+import Top_sales from './Components/Top_sales/Top_sales'
+import Top_product from './Components/Top_products/Top_products'
 
 function App() {
 
@@ -18,12 +23,52 @@ function App() {
     }
   }
 
+// debugger
+  let  slider_imgs = [sale,shopping,women]
+  let [index ,setindex] = useState(0)
+  let [img ,setimg] = useState(slider_imgs[index])
+    
+    
 
-  return(<div className={`${drk_btn}`}>
+    
+    
+    const onrightclick = ()=>{
+    
+      if(index >=3){
+        setindex(0)
+      }else{
+        setindex(1+index)
+        setimg(slider_imgs[index])
+        console.log(index)
+      }
+     
+    }
+
+
+    
+    const onleftclick = ()=>{
+      console.log(slider_imgs.length)
+      if(index <0){
+        setindex(3)
+      }else{
+        setindex(index-1)
+        setimg(slider_imgs[index])
+        console.log(index)
+      }
+
+    }
+
+
+  return(
+   
+  <div className={`${drk_btn}`}>
     <Navbar drk_btn={drk_btn} dark_Md_on={dark_Md_on}></Navbar>
-    <Slider></Slider>
-  
-  </div>)
+    <Slider img={img} onrightclick={onrightclick} onleftclick={onleftclick}></Slider>
+    <Top_sales></Top_sales>
+    <Top_product></Top_product>
+  <div className='mt-[225px]'></div>
+  </div>
+  )
 }
 
 export default App
--- FRONTEND/CLASS_ASS/REACT/shopping/src/App.jsx
+++ FRONTEND/CLASS_ASS/REACT/shopping/src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react"
 import "./App.css"
 import Navbar from "./component/Navbar/Navbar"
+import Catagory_container from "./component/Catagory_container/Catagory_container"
   
 
 
 const App =()=>{
 
-  const menu = ["shop","men","women","kids"]
+  const menu = ["home","about","contact","help"]
+  const catagory = ["men","women","kids"]
 
-
+  const display_data = ()=>{
+    console.log("ok")
+}
 
   return(
-    <>
+    <div className="container">
      <Navbar menu={menu}></Navbar>
-    <Right_bar></Right_bar>
-    </>
+     <Catagory_container catagory={catagory} display_data={display_data}></Catagory_container>
+    </div>
   )
 }
 
