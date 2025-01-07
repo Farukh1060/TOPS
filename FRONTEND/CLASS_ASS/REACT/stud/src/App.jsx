7bd7e37f490dca3eb135c1b6b2b9102b609e10b2
shaikh farukh <shaikhfarukh60@gmail.com>
2025-01-07 07:14:36 +0530

    crud

--- FRONTEND/CLASS_ASS/REACT/stud/src/App.jsx
+++ FRONTEND/CLASS_ASS/REACT/stud/src/App.jsx
@@ -5,106 +5,115 @@ import Title from "./component/Title/Title";
 import styles from "./App.module.css";
 
 import { db } from "./firestore";
-import { addDoc, collection, deleteDoc, getDocs ,doc, updateDoc, getDoc} from "firebase/firestore";
-
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  getDoc,
+} from "firebase/firestore";
 
 const detail = [
   {
     name: "xyz",
     email: "xyz@email.com",
     number: 456123789,
-  }, 
+  },
   {
     name: "xyzuvw",
     email: "xyzuvw@email.com",
     number: 4561237123,
-  }
+  },
 ];
 const App = () => {
+  const studentref = collection(db, "students");
 
-  const studentref = collection(db,"students")
-
-  let [details,setdetails] = useState([]);
-  let [student ,setstudent] = useState({})
+  let [details, setdetails] = useState([]);
+  let [student, setstudent] = useState({});
   // console.log(details)
 
   // get data from firestore and set to setdetails
 
-  const getstudent = async ()=>{
-
-    try{
-
-      const studentrefdata = await getDocs(studentref)
-      const student_data = studentrefdata.docs.map((doc)=>{
-  
+  const getstudent = async () => {
+    try {
+      const studentrefdata = await getDocs(studentref);
+      const student_data = studentrefdata.docs.map((doc) => {
         // console.log(doc.data())
-       return {
-         id : doc.id,
-        ...doc.data()}
-  
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
       });
-      setdetails(student_data)
-    }catch(error){
-      console.log(error)
+      setdetails(student_data);
+    } catch (error) {
+      console.log(error);
     }
-
-  }
-
- 
-
-
-
-//get data from form and set to firstore
-  const resiveformdata = async (studdata) => {
-    console.log(studdata.id);
-    
-
-if(studdata.id != undefined){
-
-  await updateDoc(doc(studentref,studdata.id),studdata)
-
-}else{
-  
-  await addDoc(studentref,studdata)
-}
-
-    
   };
 
-//delete data from firestore 
-  const removebtnhandler = async (id)=>{
-
-    await deleteDoc(doc(studentref,id))
-    getstudent()
+  //get data from form and set to firstore
+  const resiveformdata = async (studdata ) => {
+    // console.log(studdata);
     
+
+    if(studdata.id !=undefined){
+      const data = doc(studentref,studdata.id)
+      console.log(studdata)
+    await  updateDoc(data,{name:studdata.name,email:studdata.email,number:studdata.number})
+    }else{
+
+      await addDoc(studentref, studdata);
+    }
+
+
+    getstudent();
+  };
+
+  //delete data from firestore
+  const removebtnhandler = async (id) => {
+    // console.log("ok delete");
+
+    await deleteDoc(doc(studentref, id));
+    getstudent();
+
     // const newstuddata = details.filter(element =>(element.id != id))
     // setdetails(newstuddata)
-  }
+  };
 
-  const editbtnhandler =async (id)=>{
-const getsnap = await getDoc(doc(studentref,id))
+  const editbtnhandler = async (id) => {
+    // console.log(id);
     
-
-setstudent({name :getsnap.data().name,email:getsnap.data().email,number:getsnap.data().number ,id:getsnap.id});
-
-
-    console.log("ok")
-  }
-
-
-  useEffect( ()=>{
- 
-    console.log("123")
-    getstudent()
+    const getsnap = await getDoc(doc(studentref, id));
+    // console.log(getsnap.data());
     
-   },[])
-  
-  return (<div className={styles.container}>
-    <Title></Title>
-    <Input_field resiveformdata={resiveformdata} student={student}></Input_field>
-    <Stud_detail details={details} removebtnhandler={removebtnhandler} editbtnhandler={editbtnhandler}></Stud_detail>
-  </div>
-    
+    setstudent({
+      name: getsnap.data().name,
+      email: getsnap.data().email,
+      number: getsnap.data().number,
+      id: getsnap.id,
+    });
+  };
+
+  useEffect(() => {
+    console.log("123");
+    getstudent();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Title></Title>
+      <Input_field
+        editbtnhandler={editbtnhandler}
+        resiveformdata={resiveformdata}
+        student={student}
+      ></Input_field>
+      <Stud_detail
+        details={details}
+        removebtnhandler={removebtnhandler}
+        editbtnhandler={editbtnhandler}
+      ></Stud_detail>
+    </div>
   );
 };
 
--- FRONTEND/CLASS_ASS/REACT/stud/src/component/Input_field/Input_field.jsx
+++ FRONTEND/CLASS_ASS/REACT/stud/src/component/Input_field/Input_field.jsx
@@ -1,27 +1,26 @@
 import { useEffect, useState } from "react";
 
-const Input_field = ({ resiveformdata,student}) => {
+const Input_field = ({student, resiveformdata , editbtnhandler}) => {
 
-  // console.log(student)
   let [name, setname] = useState("");
   let [email, setemail] = useState("");
   let [number, setnumber] = useState("");
+  let [id ,setid] = useState("")
 
   useEffect(()=>{
-    console.log("abc")
-
-    if(student.id !=undefined){
-      setname(student.name)
-      setemail(student.email)
-      setnumber(student.number)
-    }
-
+    setname(student.name)
+    setemail(student.email)
+    setnumber(student.number)
+    setid(student.id)
   },[resiveformdata])
 
-  const inputnamehandler = (e) => {
-    
-      setname(e.target.value);
+  const idhandler = ()=>{
+    setid(e.target.value)
+  }
 
+
+  const inputnamehandler = (e) => {
+    setname(e.target.value);
   };
   const inputemailhandler = (e) => {
     setemail(e.target.value);
@@ -34,56 +33,68 @@ const Input_field = ({ resiveformdata,student}) => {
   };
 
   const formdata = () => {
-    let studdata = {
-      name,
-      email,
-      number
-    };
-    // console.log(studdata)
-    resiveformdata(studdata);
-    setname("")
-    setemail("")
-    setnumber("")
+    
+    if(student.id != undefined){
+  console.log(student)
+  console.log("ok")
+  resiveformdata(student)
+
+}else{
+
+  let studdata = {
+    name,
+    email,
+    number,
+  };
+  // console.log(studdata)
+  resiveformdata(studdata);
+  setname("");
+  setemail("");
+  setnumber("");
+
+}
+   
   };
 
-  return (<div>
-    <div className="row">
-      <div className="col-12">
-        <form action="" onSubmit={submit}>
-          <div className="row">
-            <div className="col-3">
-              <input
-                type="text"
-                placeholder="enter student name"
-                onChange={inputnamehandler}
-                value={name}
-              />
+  return (
+    <div>
+      <div className="row">
+        <div className="col-12">
+          <form action="" onSubmit={submit}>
+            <div className="row">
+              <input type="text" value={id} onChange={idhandler}/>
+              <div className="col-3">
+                <input
+                  type="text"
+                  placeholder="enter student name"
+                  onChange={inputnamehandler}
+                  value={name}
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="text"
+                  placeholder="enter email id"
+                  onChange={inputemailhandler}
+                  value={email}
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="text"
+                  placeholder="enter number"
+                  onChange={inputnumberhandler}
+                  value={number}
+                />
+              </div>
+              <div className="col-3">
+                <button onClick={formdata}>enter</button>
+              </div>
             </div>
-            <div className="col-3">
-              <input
-                type="text"
-                placeholder="enter email id"
-                onChange={inputemailhandler}
-                value={email}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="text"
-                placeholder="enter number"
-                onChange={inputnumberhandler}
-                value={number}
-              />
-            </div>
-            <div className="col-3">
-              <button onClick={formdata}>enter</button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-    
   );
 };
 
