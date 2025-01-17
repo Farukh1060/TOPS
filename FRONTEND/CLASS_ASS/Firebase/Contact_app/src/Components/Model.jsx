a9ce9927f8368f5cbb57f147cb61dd76353cad14
shaikh farukh <shaikhfarukh60@gmail.com>
2025-01-17 08:59:57 +0530

    firebase_auth

--- FRONTEND/CLASS_ASS/Firebase/Contact_app/src/Components/Model.jsx
+++ FRONTEND/CLASS_ASS/Firebase/Contact_app/src/Components/Model.jsx
@@ -1,27 +1,30 @@
 
+import { useEffect } from "react"
 import{IoCloseCircleOutline} from "react-icons/io5"
 
 
 
-const Model = ({close_model,name,email})=>{
+const Model = ({close_model,name,email,addcontact ,contact})=>{
 
-
-    
-
-    
-
+    useEffect(()=>{
+        if(contact.id != undefined){
+             name.current.value = contact.name,
+             email.current.value = contact.email
+            //  id.current.value = contact.id
+        }
+    },[addcontact,close_model])
 
 
     return(
 
        <>
-       <div className="p-4 relative z-50 rounded-lg min-h-[200px] max-w-[300px] bg-gray-300 m-auto ">
+       <div className="p-4 absolute top-1/2 left-1/2 -translate-x-1/2 z-50 rounded-lg min-h-[200px] max-w-[500px] bg-gray-300 m-auto ">
 
         <form action="" className="flex flex-col gap-2" onSubmit={addcontact}>
             <IoCloseCircleOutline className="self-end text-xl mb-3 cursor-pointer" onClick={close_model}/>
             <input type="text" placeholder="NAME" className="w-full outline-0 rounded-md" ref={name}/>
             <input type="text" placeholder="EMAIL" className="w-full outline-0 rounded-md" ref={email}/>
-            <button className=" bg-orange-500">add </button>
+            <button className=" bg-orange-500">{contact?"update":"add"}</button>
         </form>
 
        </div>
--- FRONTEND/CLASS_ASS/Firebase/Contact_app/src/firebase.js
+++ FRONTEND/CLASS_ASS/Firebase/Contact_app/src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from firebase/firestore
+import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBcGPgsDdSRwfHMJITJ5CBcMSTKxPXZRYk",
-  authDomain: "contact-app-3856e.firebaseapp.com",
-  projectId: "contact-app-3856e",
-  storageBucket: "contact-app-3856e.firebasestorage.app",
-  messagingSenderId: "915092801434",
-  appId: "1:915092801434:web:15e2b7f2a70fa01c899c23"
+  apiKey: "AIzaSyDZ5bIwhCq5YmBgPIodrnmJZjo-Nn1qhUg",
+  authDomain: "contact-app-19e0c.firebaseapp.com",
+  projectId: "contact-app-19e0c",
+  storageBucket: "contact-app-19e0c.firebasestorage.app",
+  messagingSenderId: "178281908631",
+  appId: "1:178281908631:web:37e8e5c11a894821c3dcd2"
 };
 
 // Initialize Firebase
- export const app = initializeApp(firebaseConfig);
- export const db = getFirestore(app)
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
